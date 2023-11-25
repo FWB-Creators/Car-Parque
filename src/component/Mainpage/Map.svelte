0b02e89b8f2b65688c6f97db19b80236d749f258
Billy19191 <bill.ratchanon@gmail.com>
2023-11-25 23:51:12 +0700

    feat: final mainpage with map

--- src/component/Mainpage/Map.svelte
+++ src/component/Mainpage/Map.svelte
@@ -1,21 +1,102 @@
 <script>
-  import 'leaflet/dist/leaflet.css'
-  //   import L from 'leaflet'
-  import { onMount } from 'svelte'
+  import { onMount, createEventDispatcher } from 'svelte'
   import { browser } from '$app/environment'
+
+  export let newData = []
+  const dispatch = createEventDispatcher()
+
+  function haversineDistance(lat1, lon1, lat2, lon2) {
+    // Radius of the Earth in kilometers
+    const R = 6371
+
+    // Convert latitude and longitude from degrees to radians
+    const dLat = (lat2 - lat1) * (Math.PI / 180)
+    const dLon = (lon2 - lon1) * (Math.PI / 180)
+
+    // Calculate the Haversine distance
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(lat1 * (Math.PI / 180)) *
+        Math.cos(lat2 * (Math.PI / 180)) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2)
+
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+
+    const distance = R * c // Distance in kilometers
+
+    return distance
+  }
 
   onMount(async () => {
     try {
       const L = await import('leaflet')
+      await import('leaflet/dist/leaflet.css')
       if (browser && L) {
-        console.log('browser')
-        const map = L.map('map').setView([51.505, -0.09], 13)
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(
-          map
-        )
+        console.log('Leaflet library loaded successfully')
+
+        const map = L.map('map').setView([0, 0], 13)
+        console.log('Map initialized:', map)
+
+        const dataMall = await fetch('Mall.json')
+        const data = await dataMall.json()
+
+        // Check if Geolocation is supported by the browser
+        if ('geolocation' in navigator) {
+          navigator.geolocation.getCurrentPosition(
+            (position) => {
+              const { latitude, longitude } = position.coords
+              console.log('Geolocation success:', latitude, longitude)
+
+              map.setView([latitude, longitude], 13)
+
+              // Add the tile layer outside the loop
+              L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map)
+
+              data.forEach((mall) => {
+                const distance = Math.sqrt(
+                  Math.pow(mall.Latitude - latitude, 2) +
+                    Math.pow(mall.Longitude - longitude, 2)
+                )
+                const distanceInKm = haversineDistance(
+                  mall.Latitude,
+                  mall.Longitude,
+                  latitude,
+                  longitude
+                )
+                console.log(
+                  'Adding marker for',
+                  mall['Mall Location'],
+                  'at',
+                  mall.Latitude,
+                  mall.Longitude
+                )
+
+                L.marker([mall.Latitude, mall.Longitude])
+                  .addTo(map)
+                  .bindPopup(mall['Mall Location'])
+                  .openPopup()
+
+                newData.push({
+                  name: mall['Mall Location'],
+                  distance: distance,
+                  distanceInKm: distanceInKm,
+                })
+              })
+
+              console.log('Markers added to the map:', newData)
+              dispatch('sendData', { newData })
+            },
+            (error) => {
+              console.error('Unable to retrieve your location', error)
+            }
+          )
+        } else {
+          console.error('Geolocation is not supported by your browser')
+        }
       }
     } catch (err) {
-      console.log('error' + err)
+      console.error('Error:', err)
     }
   })
 </script>
--- src/routes/mainpage/+page.svelte
+++ src/routes/mainpage/+page.svelte
@@ -1,16 +1,60 @@
 <script>
   import Navbar from '../../component/Navbar.svelte'
   import Map from '../../component/Mainpage/Map.svelte'
-  import { onMount } from 'svelte'
+  import { onMount, createEventDispatcher } from 'svelte'
   import { browser } from '$app/environment'
+  let loading = true
+  let passingDataLoading = true
+  let newData = []
+  let mockStore = [
+    { name: 'Central', status: '' },
+    { name: 'Big C', status: '' },
+    { name: 'Tesco Lotus', status: '' },
+    { name: 'Makro', status: '' },
+    { name: 'HomePro', status: '' },
+    { name: 'Robinson', status: '' },
+    { name: 'Mega Bangna', status: '' },
+    { name: 'Mega Bangyai', status: '' },
+  ]
 
-  const mockStore = ['Central', 'Big C', 'Tesco Lotus', 'Makro', 'HomePro']
+  const mockStatus = ['Moderate', 'High', 'Low']
+
+  const randomStatus = () => {
+    return mockStatus[Math.floor(Math.random() * mockStatus.length)]
+  }
+
+  let tempData
+  const handlePassingData = (event) => {
+    tempData = event.detail.newData
+    tempData = tempData.sort((a, b) => a.distanceInKm - b.distanceInKm)
+    console.log(tempData)
+    passingDataLoading = false
+  }
+
+  let isReady = false
+
+  onMount(async () => {
+    mockStore.forEach((store) => {
+      store.status = randomStatus()
+    })
+    // const dataMall = await fetch('Mall.json')
+    // const data = await dataMall.json()
+    // data.forEach((mall) => {
+    //   const distance = Math.sqrt(
+    //     Math.pow(mall.Latitude, 2) + Math.pow(mall.Longitude, 2)
+    //   )
+    //   console.log('root', distance)
+    // })
+    loading = false // Set isReady to true after generating random statuses
+  })
+
+  // Use reactive statement to trigger rendering after statuses are ready
 </script>
 
 <Navbar />
-<div class="relative">
+<div class="relative h-[calc(screen-4rem)] overflow-hidden">
   <div class="relative z-0">
-    <Map />
+    <Map bind:newData on:sendData={handlePassingData} />
   </div>
   <!-- search -->
   <div class="absolute top-0 left-1/2 right-1/2 z-10">
@@ -24,8 +68,36 @@
       </div>
     </div>
   </div>
-  <div class="mt-4 text-center font-bold">Nearby Department Store</div>
-  {#each mockStore as store}
-    <div>{store}</div>
-  {/each}
+  {#if loading == false}
+    <div class="mt-6 mb-2 text-center font-bold text-lg">
+      Nearby Department Store
+    </div>
+
+    <div class="h-64 overflow-scroll">
+      {#if passingDataLoading == false}
+        {#each tempData as { name, distanceInKm }}
+          <div class="relative flex flex-row border-b-2 mx-4 py-3">
+            <img src="location_icon.png" width="20" />
+            <div class="ml-2 font-medium">{name}</div>
+            <div class="absolute right-2">
+              {distanceInKm.toFixed(2)} km
+            </div>
+            <!-- {#if distance === 'Moderate'}
+              <div class="absolute right-2 text-yellow-500">{distance}</div>
+            {:else if distance === 'High'}
+              <div class="absolute right-2 text-red-500">{distance}</div>
+            {:else if distance === 'Low'}
+              <div class="absolute right-2 text-green-500">{distance}</div>
+            {/if} -->
+          </div>
+        {/each}
+      {/if}
+    </div>
+  {:else}
+    <div class="flex justify-center items-center h-fit">
+      <div
+        class="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-gray-900"
+      ></div>
+    </div>
+  {/if}
 </div>
