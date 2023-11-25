ca256c4a39e5008664edc5315fd90b2c4b6c1742
Billy19191 <bill.ratchanon@gmail.com>
2023-11-25 14:25:13 +0700

    feat: loading route

---
+++ src/routes/homepage/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+</script>
+
+<div class="flex flex-col h-screen justify-center items-center">
+  <h1 class="font-semibold text-lg">Car Parque</h1>
+  <div class="mt-2 animate-pulse">Loading..</div>
+</div>
